6ea99288f3729148a68e4e123425431e2530499d
unknown <guilherme.cpmg@hotmail.com>
2022-10-14 13:28:07 -0300

    aulas react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import './index.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./views/App";
+import DataContext, { data } from "./data/DataContext";
 
-import App from './views/App'
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useReducer } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+const initialState = {
+  other: "...",
+  products: [],
+  user: null,
+
+  //foco ...
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "number_add2":
+      return { ...state, number: state.number + 2 };
+    case 'login'  :
+        return { ...state, user: {name: action.payload}}
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+      {state.user ? <span className="text">{state.user.name}</span> : <span className="text">Sem usuário</span> }
+      
+        <div className="span text">{state.number}</div>
+        <div>
+            <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Dine'})}>Login</button>
+            <button className="btn" onClick={() => dispatch({type: 'number_add2'})}> + 2</button>
+            <button className="btn" onClick={() => dispatch({type: 'number_multiply7'})}> *7</button>
+            <button className="btn" onClick={() => dispatch({type: 'number_divide25'})}> /25</button>
+            <button className="btn" onClick={() => dispatch({type: 'number_parse'})}>Parse</button>
+            <input type="number" className="input" />
+            <button className="btn" onClick={() => dispatch({type: 'number_addN'})}>Add N</button>
+
+
+
+        </div>
+      </div>                
+    </div>
+  );
+};
+
+export default UseReducer;
